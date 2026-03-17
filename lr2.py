c0b78df5d6fdec2e150964794bbd1bc69320d918
Versus <semakov.v@edu.narfu.ru>
2026-03-17 23:34:29 +0300

    Create lr2.py

---
+++ lr2.py
@@ -0,0 +1,24 @@
+# 1
+# lst = [0, 1]
+# sm = 0
+# n = 0
+# def func_for(lst1, sm1):
+#     for i in lst1:
+#         sm1 += i
+#     return sm1
+# print(func_for(lst, sm))
+# def func_while(lst2, sm2, n2):
+#     while n2 != len(lst2):
+#         sm2 += lst2[n2]
+#         n2 += 1
+#     return sm2
+# print(func_while(lst, sm, n))
+# def func_return(lst3, sm3, n3):
+#     if n3 != len(lst3):
+#         sm3 += lst3[n3]
+#         n3 += 1
+#         return func_return(lst3, sm3, n3)
+#     else:
+#         return sm3
+# print(func_while(lst, sm, n))
+# 2
