e5e4d0bf1342bff3dc54338ae2df41379e2a0437
semakovv <semakov.v@edu.narfu.ru>
2026-04-07 22:55:01 +0300

    123.py

--- 123.py
+++ 123.py
@@ -1,7 +1,11 @@
-def foo(n):
-    def boo(x):
-        return x + n
-    return boo
-
-zoo = foo(100)
-zoo(200)
+lst = [3, 3, 3, 1, 1, 0]
+ln = len(lst)
+print(ln)
+cnt = 0
+for i in range(ln - 1, -1, -1):
+    print(lst[i])
+    for j in range(i - 1, -1, -1):
+        print(lst[i], lst[j])
+        if lst[i] == lst[j]:
+            cnt += 1
+print(cnt)
