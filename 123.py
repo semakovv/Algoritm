8b7ca4a34c3ba50b7b25f22ea2fb4eb384b93be8
Versus <semakov.v@edu.narfu.ru>
2026-04-08 21:50:27 +0300

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,13 +1,12 @@
-A = set()
-NUM = 123
-# ln_int_num = len(str(NUM))
-# print(ln_int_num)
-# for i in range(1, ln_int_num + 1):
-#     print(i)
-#     ln_int_num -= 1
-#     print(ln_int_num)
-#     A.add((NUM // 10 ** ln_int_num) % 10 ** i)
-#     print(A)
-print((NUM // 10 ** 2) % 10 ** 1)
-print((NUM // 10 ** 1) % 10 ** 1)
-print((NUM // 10 ** 0) % 10 ** 1)
+s = "1 2 3 4 5"
+A = set(s)
+print(A)
+for i in A:
+    cnt = 0
+    for j in s:
+        if i is j:
+            cnt += 1
+    if cnt >= 2:
+        print(i, cnt, "YES")
+    else:
+        print(i, cnt, "NO")
