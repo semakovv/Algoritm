556b1ddacde93428648654b8d1a40155590c0e84
semakovv <semakov.v@edu.narfu.ru>
2026-04-15 19:53:22 +0300

    Update lr2.py

--- lr2.py
+++ lr2.py
@@ -1,31 +1,34 @@
 # 1
-# lst = [0, 1]
+# lst = [0, 1, 2, 3]
 # sm = 0
 # n = 0
-# def func_for(lst1, sm1):
+# def func_for(lst1):
+#     sm1 = 0
 #     for i in lst1:
 #         sm1 += i
 #     return sm1
-# print(func_for(lst, sm))
-# def func_while(lst2, sm2, n2):
+# print(func_for(lst))
+# def func_while(lst2):
+#     sm2 = 0
+#     n2 = 0
 #     while n2 != len(lst2):
 #         sm2 += lst2[n2]
 #         n2 += 1
 #     return sm2
-# print(func_while(lst, sm, n))
-# def func_return(lst3, sm3, n3):
+# print(func_while(lst))
+# def func_return(lst3, sm3 = 0, n3 = 0):
 #     if n3 != len(lst3):
 #         sm3 += lst3[n3]
 #         n3 += 1
 #         return func_return(lst3, sm3, n3)
 #     else:
 #         return sm3
-# print(func_while(lst, sm, n))
+# print(func_while(lst))
 # 2
 # import random
 # lst = [random.randint(-9, 9) for i in range(random.randrange(10))]
 # print(lst)
-# # lst = [3, 2, 1, 0]
+# # lst = [3, 2, 1, 0] # сложность алгоритма и быстрая сортировка
 # def sort(LST):
 #     swap = False
 #     ln_lst = len(LST) - 1
@@ -39,8 +42,8 @@
 #         return LST
 # print(sort(lst))
 # 3
-# import random
-# lst = [random.randint(-9, 9) for i in range(random.randrange(10))]
+# import random #число сочитаний из НПК или кол-во рёбер в полном графе
+# lst = [random.randint(5, 9) for i in range(random.randrange(10))]
 # print(lst)
 # # lst = [3, 3, 3, 1, 1, 0]
 # def count_twice(LST):
@@ -54,7 +57,7 @@
 #     return cnt
 # print(count_twice(lst))
 # 4
-# import random
+# import random # str to set
 # num = random.randint(0, 999)
 # print(num)
 # def int_num(NUM):
@@ -68,7 +71,7 @@
 #     return A, ln_A
 # print(int_num(num))
 # 5
-# s = "1 2 3 4 5 5"
+# s = "1 2 3 4 5 5" #
 # A = set(s)
 # print(A)
 # for i in A:
@@ -81,6 +84,6 @@
 #     else:
 #         print(i, cnt, "NO")
 # 6
-# s = "a a a b b b c c c 1 2 3"
-# cnt = {i: s.count(i) for i in s}
-# print(cnt)
+s = "a a a b b b c c c 1 2 3"
+cnt = {i: s.count(i) for i in s}
+print(cnt)
