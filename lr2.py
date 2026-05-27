1e4f73b04665788509fcbb8849e57342c603f9fe
semakovv <semakov.v@edu.narfu.ru>
2026-05-27 08:29:28 +0300

    Update lr2.py

--- lr2.py
+++ lr2.py
@@ -25,26 +25,26 @@
 #         return sm3
 # print(func_while(lst))
 # 2 оценка сложности алгоритма и быстрая сотрировка O(logn)
-# import random as ra
-# lst = [ra.randint(-9, 9) for i in range(20)]
-# print(lst)
-# l = 0
-# r = len(lst)
-# def sort(LST):
-#     i = 0
-#     j = len(LST) - 1
-#     q = 0
-#     while i <= j:
-#         while i < len(LST) and LST[i] < q:
-#             i += 1
-#         while j > 0 and LST[j] > q:
-#             j -= 1
-#         if i <= j:
-#             LST[i], LST[j] = LST[j], LST[i]
-#             i += 1
-#             j -= 1
-# sort(lst)
-# print(lst)
+import random as ra
+lst = [ra.randint(-9, 9) for i in range(20)]
+print(lst)
+l = 0
+r = len(lst)
+def sort(LST):
+    i = 0
+    j = len(LST) - 1
+    q = 0
+    while i <= j:
+        while i < len(LST) and LST[i] < q:
+            i += 1
+        while j > 0 and LST[j] > q:
+            j -= 1
+        if i <= j:
+            LST[i], LST[j] = LST[j], LST[i]
+            i += 1
+            j -= 1
+sort(lst)
+print(lst)
 # 3
 # import random #число сочитаний из НПК или кол-во рёбер в полном графе
 # lst = [random.randint(5, 9) for i in range(random.randrange(10))]
@@ -61,15 +61,15 @@
 #     return cnt
 # print(count_twice(lst))
 # 4 str to set
-import random
-num = random.randint(0, 99999)
-print(num)
-def int_num(NUM):
-    NUM = str(NUM)
-    A = set(NUM)
-    for i in A:
-        print(i, NUM.count(i))
-int_num(num)
+# import random
+# num = random.randint(0, 99999)
+# print(num)
+# def int_num(NUM):
+#     NUM = str(NUM)
+#     A = set(NUM)
+#     for i in A:
+#         print(i, NUM.count(i))
+# int_num(num)
 # 5 
 # s = "1 2 3 22 22 4 5 5"
 # s = s.split()
